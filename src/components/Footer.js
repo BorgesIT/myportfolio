e90743fa226ca8b5001d3f219ead7b24c7718570
Jose Alejandro Borges <115802267+BorgesIT@users.noreply.github.com>
2022-10-31 17:20:09 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/jose-borges-4711a9254/"
+                href="https://www.linkedin.com/in/josea-borgesit/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
